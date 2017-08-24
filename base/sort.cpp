7cf0e6c2f7e8f5e5b7feb561b9b335710acfa924
leiyunfei <yunfei_lei@126.com>
2017-08-24 19:41:48 +0800

    sort

---
+++ base/sort.cpp
@@ -0,0 +1,67 @@
+/***************************************************************************
+ *
+ * Copyright © 2016 LT. All Rights Reserved.
+ *
+ ***************************************************************************/
+
+/**
+ *
+ * @file: sort.cpp
+ *
+ * @breaf: 
+ *
+ * @author: Lei Yunfei(towardstheway@gmail.com)
+ *
+ * @create: 2017/08/24 11时41分56秒
+ *
+ **/
+
+#include "lt_sort.h"
+
+#include <iostream>
+#include <vector>
+#include <functional>
+
+using namespace std;
+
+template<class T>
+struct AscendingSort {
+    bool operator()(const T &a, const T &b) {
+        return a < b;
+    }
+};
+
+
+template<class T>
+struct DescendingSort {
+    bool operator()(const T &a, const T &b) {
+        return a > b;
+    }
+};
+
+bool less(int a, int b) {
+    return a < b;
+}
+
+bool greater(int a, int b) {
+    return a > b;
+}
+
+int main(int argc, char *argv[])
+{
+    vector<int> v = {3, 2, 9, 7, 6, 5, 8, 1};
+    lt::SortWrapper::InsertSort<int>(v, DescendingSort<int>());
+    for (auto i : v) {
+        std::cout << i << '\t';
+    }
+    cout << endl;
+    lt::SortWrapper::InsertSort<int>(v);
+    for (auto i : v) {
+        std::cout << i << '\t';
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
