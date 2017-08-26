9a8480e95be5d6ce8250c707f857a4aaf8fca409
leiyunfei <yunfei_lei@126.com>
2017-08-26 17:35:47 +0800

    makefile

---
+++ define.mk
@@ -0,0 +1,24 @@
+#############################################################################
+#
+# Generic defination for Makefile
+#
+# Author:  yun63<thewardsthe@mail.com> 
+#
+#############################################################################
+
+## 源文件类型
+SRCEXTS := .c .cc .cpp .cxx
+
+## 指定C/C++编译器
+CC := gcc
+CXX := g++
+LINK := g++
+AR := ar
+RANLIB := ranlib
+
+## 编译器选项
+CFLAGS := -Wall -fPIC
+CXXFLAGS := --std=c++11 -Wall -fPIC
+MYCFLAGS := -DDEBUG
+ARFLAGS := rcs
+
