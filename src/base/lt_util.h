b9514deaec8cd59cdf88a3a77dded2d83dc87392
yun63 <towardstheway@gmail.com>
2017-12-25 07:06:58 +0800

    sring大小写处理函数

--- src/base/lt_util.h
+++ src/base/lt_util.h
@@ -92,6 +92,20 @@ void random_shuffle(std::vector<T> &array) {
     }
 }
 
+std::string toupper(std::string s) {
+    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {
+            return std::toupper(c);
+            });
+    return s;
+}
+
+std::string tolower(std::string s) {
+    std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c) {
+            return std::tolower(c);
+            });
+    return s;
+}
+
 } // namespace lt
 
 #endif   // ----- #ifndef LT_UTIL_INC  -----
