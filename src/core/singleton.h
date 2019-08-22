4ec4f66ff46ef7ab78d3b344ab495a515c7eb9a1
yun63 <yun63@126.com>
2019-08-22 18:04:37 +0800

    singleton

--- src/core/singleton.h
+++ src/core/singleton.h
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * Copyright © 2016 LT. All Rights Reserved.
+ * Copyright © 2019 LT. All Rights Reserved.
  *
  ***************************************************************************/
 
@@ -8,18 +8,68 @@
  *
  * @file: singleton.h
  *
- * @breaf: 单例类
+ * @breaf: 
  *
  * @author: Lei Yunfei(towardstheway@gmail.com)
  *
- * @create: 2017/10/16 15时10分01秒
+ * @create: 2019/08/21 18时08分55秒
  *
  **/
 
+#include <cassert>
+#include <pthread.h>
+#include "noncopyable.h"
 
-#ifndef  SINGLETON_INC
-#define  SINGLETON_INC
 
-#include "base/noncopyable.h"
+template<typename T>
+class Singleton : noncopyable {
+public:
+    Singleton() = delete;
+    ~Singleton() = delete;
 
-#endif   // ----- #ifndef SINGLETON_INC  -----
+    static T &GetInstance();
+    static T *GetInstancePtr();
+
+private:
+    static void init();
+    static void destroy();
+
+    static T *instance_;
+    static pthread_once_t thread_once_;
+};
+
+template<typename T>
+T &Singleton<T>::GetInstance() {
+    pthread_once(&thread_once_, &Singleton<T>::init);
+    assert(instance_ != nullptr);
+    return *instance_;
+}
+
+
+template<typename T>
+T *Singleton<T>::GetInstancePtr() {
+    pthread_once(&thread_once_, &Singleton<T>::init);
+    assert(instance_ != nullptr);
+    return instance_;
+}
+
+template<typename T>
+void Singleton<T>::init() {
+    instance_ = new T();
+     // ObjectManager::AtExit(destory)
+}
+
+template<typename T>
+void Singleton<T>::destroy() {
+    if (instance_) {
+        delete instance_;
+    }
+    instance_ = nullptr;
+}
+
+template<typename T>
+T *Singleton<T>::instance_ = nullptr;
+
+template<typename T>
+pthread_once_t Singleton<T>::thread_once_ = PTHREAD_ONCE_INIT;
+
